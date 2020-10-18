a26473bcf193ce146d5474c9492d37e4d92893c4
Pasquale Minervini <p.minervini@gmail.com>
2020-10-18 17:51:17 +0200

    update

--- experiments/topk_dev.py
+++ experiments/topk_dev.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-dsets = ['FB15k', 'FB15k-237', 'NELL']
+dsets = ['FB15K', 'FB237', 'NELL']
 ranks = [100, 200, 500, 1000]
 tnorms = ['min', 'product']
 candidates = [3, 4, 5]
@@ -10,7 +10,14 @@ for d in dsets:
     for r in ranks:
         for t in tnorms:
             for c in candidates:
-                cmd = f"PYTHONPATH=. python3 kbc/query_answer_BF.py --model_path models/{d}-model-rank-{r}-epoch-100-*.pt " \
+
+                m = d
+                if d == 'FB237':
+                    m = "FB15k-237"
+                elif d == 'FB15K':
+                    m = 'FB15k-237'
+
+                cmd = f"PYTHONPATH=. python3 kbc/query_answer_BF.py --model_path models/{m}-model-rank-{r}-epoch-100-*.pt " \
                     f"--dataset {d}_dev --dataset_mode test --t_norm {t} --candidates {c}"
 
                 log_path = f"logs/topk_dev_d={d}_r={r}_t={t}_c={c}.log"
