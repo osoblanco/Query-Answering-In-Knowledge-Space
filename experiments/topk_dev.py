ea7c1988fb8f7c393d22155bb94998e96d074fbb
Pasquale Minervini <p.minervini@gmail.com>
2020-10-18 17:42:29 +0200

    update

--- experiments/topk_dev.py
+++ experiments/topk_dev.py
@@ -10,7 +10,7 @@ for d in dsets:
     for r in ranks:
         for t in tnorms:
             for c in candidates:
-                cmd = f"PYTHONPATH=. python3 kbc/query_answer_BF.py --model_path models/{d}-rank-{r}-epoch-100-*.pt " \
+                cmd = f"PYTHONPATH=. python3 kbc/query_answer_BF.py --model_path models/{d}-model-rank-{r}-epoch-100-*.pt " \
                     f"--dataset {d}_dev --dataset_mode test --t_norm {t} --candidates {c}"
 
                 log_path = f"logs/topk_dev_d={d}_r={r}_t={t}_c={c}.log"
